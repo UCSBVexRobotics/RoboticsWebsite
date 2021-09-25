7856009c1c072362ba1b9671793c96795710ed48
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-25 02:36:49 -0700

    updated about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="about">
 			<h3 id="about">ABOUT US</h3>
 			<p className="text">
-				The Robotics Club at UCSB is a project-oriented club composed of students interested in the many fields related to robotics. We participate in several competitions throughout the year, as well as volunteer work, educational outreach programs, and working on other member-initiated projects. Students of all majors and skill levels with an interest in learning more about robotics are strongly encouraged to join and participate in the activities held throughout the year!
+				The Robotics Club at UCSB is a project-oriented club composed of students interested in the many fields related to robotics. We participate in competitions throughout the year, as well as do volunteer work, hold educational outreach programs, and work on other member-initiated projects. Students of all majors and skill levels with an interest in learning more about robotics are strongly encouraged to join and participate in the activities held throughout the year!
 			</p>
 		</div>
     )
